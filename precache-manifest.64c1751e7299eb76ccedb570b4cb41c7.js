2e1d53fd8589e26bab57da4605f7ca78e5eea801
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2020-08-05 10:47:37 -0400

    Updates

---
+++ precache-manifest.64c1751e7299eb76ccedb570b4cb41c7.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e940977a8a6f3954c79716ec4875064e",
+    "url": "/portf/index.html"
+  },
+  {
+    "revision": "4978c1b703c94d24955c",
+    "url": "/portf/static/css/main.2732cef8.chunk.css"
+  },
+  {
+    "revision": "6bce58a6640f676260da",
+    "url": "/portf/static/js/2.34fa3a21.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portf/static/js/2.34fa3a21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4978c1b703c94d24955c",
+    "url": "/portf/static/js/main.1c87de41.chunk.js"
+  },
+  {
+    "revision": "0700ce093da7849dd878",
+    "url": "/portf/static/js/runtime-main.f89d973a.js"
+  },
+  {
+    "revision": "1a8fbcc8bc7c27b8435c2c30b202fd7e",
+    "url": "/portf/static/media/Resume.1a8fbcc8.pdf"
+  },
+  {
+    "revision": "5bb7c8d0cf0c9824cc1762d9b4c5effd",
+    "url": "/portf/static/media/background.5bb7c8d0.png"
+  },
+  {
+    "revision": "470430322f7bb74c01040e40d29550de",
+    "url": "/portf/static/media/codingQuiz.47043032.gif"
+  },
+  {
+    "revision": "1a3a635ebceee80b0179a233cac99d7c",
+    "url": "/portf/static/media/dailyScheduler.1a3a635e.gif"
+  },
+  {
+    "revision": "8a1f5e84d84ac18605e89754b1a9bbf1",
+    "url": "/portf/static/media/employeeDirectory.8a1f5e84.png"
+  },
+  {
+    "revision": "0918d78648457def080137b179fc5608",
+    "url": "/portf/static/media/github.0918d786.png"
+  },
+  {
+    "revision": "a817b156c1150dfa6128d85412cc4ab6",
+    "url": "/portf/static/media/memoryMaker.a817b156.png"
+  },
+  {
+    "revision": "778e203548cb340ab87eedf5abc63847",
+    "url": "/portf/static/media/personal.778e2035.png"
+  },
+  {
+    "revision": "3a1a90db8e059b12ec1a45963a0afbdd",
+    "url": "/portf/static/media/resumeIcon.3a1a90db.png"
+  },
+  {
+    "revision": "07e42be211db59fa370706cdcbce0366",
+    "url": "/portf/static/media/stayingInTonight.07e42be2.png"
+  },
+  {
+    "revision": "7d1ede732636bf207385926c00128ad2",
+    "url": "/portf/static/media/uArrow.7d1ede73.png"
+  },
+  {
+    "revision": "cd909be0064f1d5d755d62d00c7952c1",
+    "url": "/portf/static/media/weatherDashboard.cd909be0.png"
+  }
+]);
